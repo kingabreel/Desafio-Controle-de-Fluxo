7f1b00c59a0f687dbbc8876b73b8dbec58532bcb
Gabriel <107805241+kingabreel@users.noreply.github.com>
2023-02-08 11:27:54 -0300

    Add files via upload

---
+++ DesafioDoControleFluxo.java
@@ -0,0 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class DesafioDoControleFluxo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int numeroUm = scanner.nextInt();
+        System.out.println("Digite mais um número, sendo este maior que o priomeiro: ");
+        int numeroDois = scanner.nextInt();
+
+        try {
+            contarDiferenca(numeroUm, numeroDois);
+        } catch (NumerosInvalidosException e){
+            System.out.println("O segundo número deve ser maior que o primeiro");
+        }
+    }
+
+    static void contarDiferenca(int numeroUm, int numeroDois)throws NumerosInvalidosException{
+        if(numeroUm > numeroDois){
+            throw new NumerosInvalidosException();
+        }
+        int contagem = numeroDois - numeroUm;
+        for (int i = 0; i <= contagem; i++){
+            System.out.println("Imprimindo o número " + i);
+        }
+        System.out.println("A diferença entre " + numeroUm + " e " + numeroDois + " é de " + contagem + " números");
+
+    }
+    static class NumerosInvalidosException extends Exception{}
+}
